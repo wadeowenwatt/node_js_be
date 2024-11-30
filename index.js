892d387abb467c49f71469cf3d457d9827708b80
wadeowenwatt <trannhatlinh120801@gmail.com>
2024-11-30 23:14:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+// Middleware
+app.use(express.json());
+
+app.get("/push_noti", (request, response) => {
+    response.status(200).send({
+        chill_guy: '🤡'
+    })
+});
+
+app.post();
+
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
