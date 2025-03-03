a6e3bac002f9202c91e7c1928905a1c5c5c32891
wadeowenwatt <trannhatlinh120801@gmail.com>
2025-03-03 23:07:20 +0700

    add base a project full stack does

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
-const express = require("express");
+import express from "express";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+
+
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5003;
 
-// Middleware
-app.use(express.json());
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the file path 
+const __dirname = dirname(__filename);
 
-app.get("/push_noti", (request, response) => {
-    response.status(200).send({
-        chill_guy: '🤡'
-    })
+// Serves the HTML file from the /public directory 
+// Tells express to serve all files from the public folder as static assets 
+// file. Any requests for the css files will be resolved to the public directory 
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Serving up the HTML file from the /public directory
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-app.listen(PORT, () =>
-  console.log(`Server running on port http://localhost:${PORT}`)
-);
